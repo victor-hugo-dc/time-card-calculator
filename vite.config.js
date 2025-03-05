0260b3964f4f07703c9c8529a2ce9b77e9e46003
Victor Del Carpio <vhdelcg@gmail.com>
2025-03-05 18:43:59 -0500

    feat: vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['@mui/x-date-pickers', '@mui/x-date-pickers/AdapterDateFns'],
+  },
+});
